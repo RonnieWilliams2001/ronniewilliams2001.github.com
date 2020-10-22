d4ca19219f0190f7ed54b0fbc75dadd3345a6250
Ronaldi2001 <ronaldi2001@live.com>
2020-10-22 08:20:45 -0400

    Update

--- JS/progress_bar.js
+++ JS/progress_bar.js
@@ -1,6 +1,11 @@
 const progressBar = document.getElementsByClassName('progress-bar')[0]
 setInterval(() => {
+
+setCookie("progress", 65, 1);
+
+	const location = getCookie("progress") || 0;
   const computedStyle = getComputedStyle(progressBar)
   const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0
-  progressBar.style.setProperty('--width', width + .1)
+  progressBar.style.setProperty('--width', location)
+
 }, 5)
