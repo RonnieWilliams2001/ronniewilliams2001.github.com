fda086db80412b15a6f2e27a694b3da4ab29d946
Ronaldi2001 <ronaldi2001@live.com>
2020-10-22 09:09:54 -0400

    Update progress_bar.js

--- JS/progress_bar.js
+++ JS/progress_bar.js
@@ -1,8 +1,20 @@
+function taskProgress() {
+
+var progressBar = document.getElementById("taskProgress");
+
+  if(getCookie("amtTasks") == null && getCookie("totalTasks") == null) {
+  	progressBar.innerHTML = "Task Progress: No Tasks";
+  } else {
+
+  	progressBar.innerHTML = "Task Progress: No Tasks";progressBar.innerHTML = "Task Progress: " +  getCookie("amtTasks") + " / " + getCookie("totalTasks");
+  	setCookie("progress", totalTasks/amtTasks*100 || 0, 1);
+  }
+}
+
 const progressBar = document.getElementsByClassName('progress-bar')[0]
-setInterval(() => {
+	setInterval(() => {
 
-setCookie("progress", 65, 1);
-
+  taskProgress();
 	const location = getCookie("progress") || 0;
   const computedStyle = getComputedStyle(progressBar)
   const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0
