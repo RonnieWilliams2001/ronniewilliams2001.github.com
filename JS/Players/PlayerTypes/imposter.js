e9a9fef0c731372657170d9c50e0b9d513975473
Ronaldi2001 <ronaldi2001@live.com>
2020-10-23 15:26:54 -0400

    Update

--- JS/Players/PlayerTypes/imposter.js
+++
@@ -1,5 +0,0 @@
-funciton getImposterAmt() {
-
-if()
-
-}
--- JS/start.js
+++ JS/start.js
@@ -1,22 +1,10 @@
-
-function getPlayerAmt() [
-
-    var playerAmt = 0;
-
-    for(var i = 1; i <16; i++) {
-
-        if(getCookie("player" + String(i))) playerAmt++;
-    }
-
-    return playerAmt;
-]
-
 function startAll() {
 
-    setCookie("gameLive", true, 1)
+    setCookie("gameLive", true, 1);
     setCookie("playerAmt", getPlayerAmt(), 1);
-    setCookie("imposterAmt", getImposterAmt(), 1);
+    setCookie("imposterOne", "", 1);
+    setCookie("imposterTwo", "", 1);
+    setImposter();
     setCookie("O2", true, 1);
 
 }
-
